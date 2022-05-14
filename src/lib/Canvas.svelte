73943e509c8bcf6d20e8791cc0a9d1a54abb6c3a
Pontus Horn <pontus.horn@prisjakt.nu>
2022-05-14 12:08:25 +0200

    Add canvas

---
+++ src/lib/Canvas.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	let canvas: HTMLCanvasElement;
+	$: ctx = canvas?.getContext('2d');
+</script>
+
+<canvas width="800" height="600" bind:this={canvas} />
+
+<style>
+	canvas {
+		width: 800px;
+		height: 600px;
+	}
+</style>
