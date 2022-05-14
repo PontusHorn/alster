8f076e9dd9e2904d1ac6e517bb18965e38e2fdf6
Pontus Horn <pontus.horn@prisjakt.nu>
2022-05-14 15:36:01 +0200

    Add tests for longer expressions

--- src/lib/expression.test.ts
+++ src/lib/expression.test.ts
@@ -99,4 +99,53 @@ describe('evaluate', () => {
 			expect(evaluate([ref('i'), op('^'), ref('j')], { i: 4, j: 2 })).toBe(16);
 		});
 	});
+
+	describe('multiple sub-expressions and order of operations', () => {
+		it('resolves multiple value additions', () => {
+			expect(evaluate([val(2), op('+'), val(3), op('+'), val(4)], {})).toBe(9);
+		});
+
+		it('resolves multiple value subtractions', () => {
+			expect(evaluate([val(2), op('-'), val(3), op('-'), val(4)], {})).toBe(-5);
+		});
+
+		it('resolves multiple value multiplications', () => {
+			expect(evaluate([val(2), op('*'), val(3), op('*'), val(4)], {})).toBe(24);
+		});
+
+		it('resolves multiple value divisions', () => {
+			expect(evaluate([val(3), op('/'), val(4), op('/'), val(2)], {})).toBe(0.375);
+		});
+
+		it('resolves multiple value remainders left to right', () => {
+			expect(evaluate([val(10), op('%'), val(6), op('%'), val(3)], {})).toBe(1);
+		});
+
+		it('resolves multiple value exponents left to right', () => {
+			expect(evaluate([val(2), op('^'), val(3), op('^'), val(4)], {})).toBe(4096);
+		});
+
+		it('resolves mixed value additions and subtractions from left to right', () => {
+			expect(evaluate([val(2), op('-'), val(3), op('+'), val(4)], {})).toBe(3);
+		});
+
+		it('resolves mixed value multiplications, divisions, and remainders from left to right', () => {
+			expect(evaluate([val(100), op('/'), val(2), op('%'), val(20), op('*'), val(3)], {})).toBe(30);
+		});
+
+		it('resolves multiplications, divisions, and remainders before additions and subtractions', () => {
+			expect(evaluate([val(2), op('+'), val(3), op('*'), val(4)], {})).toBe(14);
+			expect(evaluate([val(2), op('-'), val(3), op('*'), val(4)], {})).toBe(-10);
+			expect(evaluate([val(2), op('+'), val(3), op('/'), val(4)], {})).toBe(2.75);
+			expect(evaluate([val(2), op('-'), val(3), op('/'), val(4)], {})).toBe(1.25);
+			expect(evaluate([val(4), op('+'), val(3), op('%'), val(2)], {})).toBe(5);
+			expect(evaluate([val(4), op('-'), val(3), op('%'), val(2)], {})).toBe(3);
+		});
+
+		it('resolves exponents before multiplications, divisions, and remainders', () => {
+			expect(evaluate([val(4), op('*'), val(3), op('^'), val(2)], {})).toBe(36);
+			expect(evaluate([val(4), op('/'), val(2), op('^'), val(2)], {})).toBe(1);
+			expect(evaluate([val(7), op('%'), val(2), op('^'), val(2)], {})).toBe(3);
+		});
+	});
 });
