cc281490cd9b901ff91136e9a834deaa0d16d024
Pontus Horn <pontus.horn@prisjakt.nu>
2022-05-31 23:36:36 +0200

    Try out a new grid use case

---
+++ src/lib/presets/chains.ts
@@ -0,0 +1,48 @@
+import { makeHsl, makeRgb } from '$lib/utils/drawing';
+import { toExp } from '$lib/utils/expression';
+
+const chainsConfig = {
+	root: {
+		background: makeRgb(238, 227, 185),
+		width: 800,
+		height: 600,
+		iterationIds: ['yy35ijzr458']
+	},
+	iterations: [
+		{
+			type: 'iteration',
+			id: 'yy35ijzr458',
+			name: 'chain',
+			start: 0,
+			end: 3,
+			shapeIds: [],
+			iterationIds: ['2zeea47kpam']
+		},
+		{
+			type: 'iteration',
+			id: '2zeea47kpam',
+			name: 'link',
+			start: 0,
+			end: 120,
+			shapeIds: ['9edvd4bcgd8'],
+			iterationIds: []
+		}
+	],
+	shapes: [
+		{
+			type: 'shape',
+			shapeType: 'ellipse',
+			id: '9edvd4bcgd8',
+			color: makeHsl('$chain * 100 + $link * 122 - $time * 0.03', 80, 60),
+			x: toExp(
+				'($chain * ($canvasWidth + 100) / 3 + $link * 7 + $time / 10) % ($canvasWidth + 100) - 50'
+			),
+			y: toExp('$link * 6 - 50'),
+			width: toExp(100),
+			height: toExp(40),
+			rotation: toExp('$link * 0.06 + $time / 1000')
+		}
+	]
+};
+
+export default chainsConfig;
--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -1,5 +1,5 @@
 import type { RootConfig, Iteration, Shape } from '$lib/types';
-import { makeHsl, makeRgb } from '$lib/utils/drawing';
+import { makeNumberedColor, makeRgb } from '$lib/utils/drawing';
 import { toExp } from '$lib/utils/expression';
 import { derived, writable } from 'svelte/store';
 
@@ -14,19 +14,19 @@ export const iterations = writable<Iteration[]>([
 	{
 		type: 'iteration',
 		id: 'yy35ijzr458',
-		name: 'chain',
+		name: 'row',
 		start: 0,
-		end: 3,
+		end: 12,
 		shapeIds: [],
 		iterationIds: ['2zeea47kpam']
 	},
 	{
 		type: 'iteration',
 		id: '2zeea47kpam',
-		name: 'link',
+		name: 'column',
 		start: 0,
-		end: 120,
-		shapeIds: ['9edvd4bcgd8'],
+		end: 16,
+		shapeIds: ['9edvd4bcgd8', 'pvnkcnc4bm'],
 		iterationIds: []
 	}
 ]);
@@ -34,16 +34,25 @@ export const iterations = writable<Iteration[]>([
 export const shapes = writable<Shape[]>([
 	{
 		type: 'shape',
-		shapeType: 'ellipse',
+		shapeType: 'rectangle',
 		id: '9edvd4bcgd8',
-		color: makeHsl('$chain * 100 + $link * 122 - $time * 0.03', 80, 60),
-		x: toExp(
-			'($chain * ($canvasWidth + 100) / 3 + $link * 7 + $time / 10) % ($canvasWidth + 100) - 50'
-		),
-		y: toExp('$link * 6 - 50'),
-		width: toExp(100),
-		height: toExp(40),
-		rotation: toExp('$link * 0.06 + $time / 1000')
+		color: makeNumberedColor('$row * 12 + $column'),
+		x: toExp('$column * 50 + 25'),
+		y: toExp('$row * 50 + 25'),
+		width: toExp(50),
+		height: toExp(50),
+		rotation: toExp(0)
+	},
+	{
+		type: 'shape',
+		shapeType: 'ellipse',
+		id: 'pvnkcnc4bm',
+		color: makeNumberedColor('16 * 12 + $row * 12 + $column'),
+		x: toExp('$column * 50 + 25'),
+		y: toExp('$row * 50 + 25'),
+		width: toExp(40),
+		height: toExp(20),
+		rotation: toExp('$column * 10 + $row * 10')
 	}
 ]);
 
